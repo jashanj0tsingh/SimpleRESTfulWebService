6b4191cbe76eabfe7a9d5e2b98c43354404a1d08
Jashanjot Singh <s_jashan@orwell.encs.concordia.ca>
2020-01-29 17:39:23 -0500

    generic resource class with various hardcoded mime types

---
+++ src/java/entities/GenericResource.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author s_jashan
+ */
+@Path("generic")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+    
+    String plainText = null;
+    String html = null;
+    String xml = null;
+    String json = null;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of foo.GenericResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Path("/text")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getText() 
+    {
+        plainText = "Plain text response from RESTful web service!";
+        return plainText;
+    }
+    
+    @GET
+    @Path("/html")
+    @Produces(MediaType.TEXT_HTML)
+    public String getHTML() 
+    {
+        html = "<html> " + 
+                        "<title>" + 
+                            "Hello SOEN487" + 
+                        "</title>" +
+                        "<body><h1>" + 
+                            "HTML response from RESTful web service!" + 
+                        "</body></h1>" + 
+                      "</html> ";
+        return html;
+    }
+    
+    @GET
+    @Path("/xml")
+    @Produces(MediaType.TEXT_XML)
+    public String getXML() 
+    {
+        xml = "<?xml version=\"1.0\"?>" +
+                        "<response>" + 
+                            "XML response from RESTful web service!" +
+                        "</response>";
+        return xml;
+    }
+    
+    @GET
+    @Path("/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJSON() 
+    {
+        json = "plain text from RESTful web service!";
+        return json;
+    }
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void putText(String content) {
+    }
+}
