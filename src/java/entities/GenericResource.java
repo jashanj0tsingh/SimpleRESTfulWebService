f6c0d9aa56d17e712c3052f87c5d8836012cda14
Jashanjot Singh <s_jashan@orwell.encs.concordia.ca>
2020-01-29 20:19:44 -0500

    put method for generic text/plain

--- src/java/entities/GenericResource.java
+++ src/java/entities/GenericResource.java
@@ -11,8 +11,10 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * REST Web Service
@@ -92,9 +94,13 @@ public class GenericResource {
     /**
      * PUT method for updating or creating an instance of GenericResource
      * @param content representation for the resource
+     * @return 
      */
     @PUT
-    @Consumes(MediaType.TEXT_PLAIN)
-    public void putText(String content) {
+    @Path("/text")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response putText(String content) {
+        plainText = content;
+        return Response.status(200).entity(plainText).build();
     }
 }
